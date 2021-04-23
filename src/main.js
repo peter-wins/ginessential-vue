7c2019816b6212f07d38df607be5e923a6e65ece
peter <peterloginmanila@gmail.com>
2021-04-23 17:42:08 +0400

    Update

--- src/main.js
+++ src/main.js
@@ -1,5 +1,6 @@
 import Vue from 'vue';
 import { BootstrapVue, IconsPlugin } from 'bootstrap-vue';
+import Vuelidate from 'vuelidate';
 import App from './App.vue';
 import router from './router';
 import store from './store';
@@ -8,7 +9,7 @@ import store from './store';
 import './assets/scss/index.scss';
 
 Vue.config.productionTip = false;
-
+Vue.use(Vuelidate);
 // Make BootstrapVue available throughout your project
 Vue.use(BootstrapVue);
 // Optionally install the BootstrapVue icon components plugin
--- src/views/register/Register.vue
+++ src/views/register/Register.vue
@@ -1,34 +1,59 @@
 <template>
   <div class="register">
-    <b-card title="注册">
-    <b-form>
-      <b-form-group>
-        <b-form-input
-          v-model="user.name"
-          type="text"
-          placeholder="请输入您的昵称(选填)"
-        ></b-form-input>
-      </b-form-group>
+    <b-row class="mt-5">
+      <b-col
+        md="8"
+        offset-md="2"
+        lg="6"
+        offset-lg="3"
+      >
+        <b-card title="注册">
+          <b-form>
+            <b-form-group label="姓名">
+              <b-form-input
+                v-model="$v.user.name.$model"
+                type="text"
+                placeholder="请输入您的昵称(选填)"
+              ></b-form-input>
+            </b-form-group>
+            <b-form-group label="手机号码">
+              <b-form-input
+                v-model="$v.user.telephone.$model"
+                type="number"
+                placeholder="请输入手机号码"
+              ></b-form-input>
+              <b-form-invalid-feedback :state="validation">
+                手机号必须为 11 位
+              </b-form-invalid-feedback>
+              <b-form-valid-feedback :state="validation">
+                手机号符合 11 位
+              </b-form-valid-feedback>
+            </b-form-group>
+            <b-form-group label="密码">
+              <b-form-input
+                v-model="$v.user.password.$model"
+                type="password"
+                placeholder="请输入密码"
+              ></b-form-input>
+            </b-form-group>
             <b-form-group>
-        <b-form-input
-          v-model="user.telephone"
-          type="number"
-          placeholder="请输入手机号码"
-        ></b-form-input>
-      </b-form-group>
-            <b-form-group>
-        <b-form-input
-          v-model="user.password"
-          type="password"
-          placeholder="请输入密码"
-        ></b-form-input>
-      </b-form-group>
-    </b-form>
-    </b-card>
+              <b-button
+                @click="register"
+                variant="outline-primary"
+                block
+              >注册</b-button>
+            </b-form-group>
+          </b-form>
+        </b-card>
+      </b-col>
+    </b-row>
   </div>
 </template>
 
 <script>
+
+import { required, minLength, maxLength } from 'vuelidate/lib/validators';
+
 export default {
   data() {
     return {
@@ -37,7 +62,37 @@ export default {
         telephone: '',
         password: '',
       },
+      validation: null,
     };
+  },
+  validations: {
+    user: {
+      name: {
+        required,
+        minLength: minLength(11),
+        maxLength: maxLength(11),
+      },
+      telephone: {
+        required,
+        minLength: minLength(11),
+        maxLength: maxLength(11),
+      },
+      password: {
+        required,
+        minLength: minLength(11),
+        maxLength: maxLength(11),
+      },
+    },
+  },
+  methods: {
+    register() {
+      if (this.user.telephone.length !== 11) {
+        this.validation = false;
+        return;
+      }
+      this.validation = true;
+      console.log('register');
+    },
   },
 };
 </script>
