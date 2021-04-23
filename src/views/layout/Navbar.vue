b628f7361b48fab826080a8ceef4eb5dde13cb00
peter <peterloginmanila@gmail.com>
2021-04-23 11:39:33 +0400

    Update

---
+++ src/views/layout/Navbar.vue
@@ -0,0 +1,29 @@
+<template>
+  <div>
+  <b-navbar toggleable="lg" type="dark" variant="info">
+    <b-container>
+    <b-navbar-brand href="#">OceanLearn</b-navbar-brand>
+
+    <b-navbar-toggle target="nav-collapse"></b-navbar-toggle>
+    <b-collapse id="nav-collapse" is-nav>
+
+      <!-- Right aligned nav items -->
+      <b-navbar-nav class="ml-auto">
+
+        <b-nav-item @click="$router.replace({name: 'login'})">登录</b-nav-item>
+        <b-nav-item @click="$router.replace({name: 'register'})">注册</b-nav-item>
+      </b-navbar-nav>
+      </b-collapse>
+    </b-container>
+  </b-navbar>
+</div>
+</template>
+
+<script>
+export default {
+
+};
+</script>
+<style lang="scss">
+
+</style>
--- src/views/register/Register.vue
+++ src/views/register/Register.vue
@@ -1,11 +1,43 @@
 <template>
   <div class="register">
-      register
+    <b-card title="注册">
+    <b-form>
+      <b-form-group>
+        <b-form-input
+          v-model="user.name"
+          type="text"
+          placeholder="请输入您的昵称(选填)"
+        ></b-form-input>
+      </b-form-group>
+            <b-form-group>
+        <b-form-input
+          v-model="user.telephone"
+          type="number"
+          placeholder="请输入手机号码"
+        ></b-form-input>
+      </b-form-group>
+            <b-form-group>
+        <b-form-input
+          v-model="user.password"
+          type="password"
+          placeholder="请输入密码"
+        ></b-form-input>
+      </b-form-group>
+    </b-form>
+    </b-card>
   </div>
 </template>
 
 <script>
 export default {
-
+  data() {
+    return {
+      user: {
+        name: '',
+        telephone: '',
+        password: '',
+      },
+    };
+  },
 };
 </script>
